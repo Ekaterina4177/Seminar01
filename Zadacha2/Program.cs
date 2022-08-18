b92fa5c1dcaa49a4f537b390992d9906e53ce473
Ekaterina <you@example.com>
2022-08-18 21:32:00 +0300

    Вторая задача

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Ввведите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввведите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA == (numberB * numberB))
+{
+   Console.WriteLine("Yes");
+}
+
+else{
+   Console.WriteLine("No");
+}
