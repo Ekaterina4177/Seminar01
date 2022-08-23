fe4ddd1df840d6aa94d0db0c41d0dcc20530f6f3
Ekaterina <you@example.com>
2022-08-23 14:31:52 +0300

    Задача 3 через свитч

--- Zadacha3/Program.cs
+++ Zadacha3/Program.cs
@@ -5,11 +5,15 @@
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if(number == 3)
-{
-    Console.WriteLine("Среда");
-}
-else(number == 5)
-{
-    Console.WriteLine("Пятница");
+if(number <= 7 || number >= 1){
+    switch(number){
+case 1: Console.WriteLine("Monday"); break;
+case 2: Console.WriteLine("Tuesday"); break;
+case 3: Console.WriteLine("Wednesday"); break;
+case 4: Console.WriteLine("Thursday"); break;
+case 5: Console.WriteLine("Friday"); break;
+case 6: Console.WriteLine("Saturday"); break;
+case 7: Console.WriteLine("Sunday"); break;
+default: Console.WriteLine("Enter a number from 1 to 7"); break;
+    }
 }
